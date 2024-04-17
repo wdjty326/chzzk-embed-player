3922dc11938871c215f7da10fb51ab16ea893e87
TaekYooJeong-Toonation <tyjung@toonation.co.kr>
2024-04-17 19:33:49 +0900

    Chrone: PC 소형 사이즈 처리 추가

--- src/player.tsx
+++ src/player.tsx
@@ -3,8 +3,8 @@ import MobileDetect from 'mobile-detect';
 
 import './player.css';
 
-const md = new MobileDetect(window.navigator.userAgent);
-const mobile = md.mobile();
+const md = new MobileDetect(window.navigator.userAgent, 520);
+const mdv = md.mobile() !== null;
 
 interface ChzzkEmbedPlayerProps {
 	channelId: string;
@@ -12,28 +12,32 @@ interface ChzzkEmbedPlayerProps {
 
 export const ChzzkEmbedPlayer = ({ channelId }: ChzzkEmbedPlayerProps) => {
 	const chzzkIframeRef = useRef<HTMLIFrameElement>(null);
-	const [isChanged, setIsChanged] = useState<boolean>(false);
+	const [mobile, setMobile] = useState<boolean>(mdv);
+	const [changed, setChanged] = useState<boolean>(false);
 
-	const href = useMemo(() => `https://${mobile ? 'm.chzzk' : 'chzzk'}.naver.com/live/${channelId}`, [channelId]);
+	const href = useMemo(() => `https://${mobile ? 'm.chzzk' : 'chzzk'}.naver.com/live/${channelId}`, [mobile, channelId]);
 
 	useEffect(() => {
-		if (!chzzkIframeRef.current || mobile) return;
+		if (!chzzkIframeRef.current || mdv) return;
 		const observer = new ResizeObserver((entries) => {
 			entries.forEach((entry) => {
-				const updated = entry.contentRect.width < 1200;
-				if (updated !== isChanged) {
-					setIsChanged(updated);
+				const changeUpdated = entry.contentRect.width < 1200;
+				const mobileUpdated = entry.contentRect.width < 950;
+				if (changeUpdated !== changed) {
+					setChanged(changeUpdated);
+				} else if (mobileUpdated !== mobile) {
+					setMobile(mobileUpdated);
 				}
 			});
 		});
 		observer.observe(chzzkIframeRef.current);
 		return () => observer.disconnect();
-	}, [isChanged]);
+	}, [changed, mobile]);
 
-	return (<div className="ChzzkPlayer">
+	return (<div className="ChzzkPlayer"	>
 		<div className={`ChzzkPlayerWrapper ${mobile ? 'Mobile' : ''}`}>
 			<iframe
-				className={!mobile && isChanged ? 'Updated' : ''}
+				className={!mobile && changed ? 'Updated' : ''}
 				ref={chzzkIframeRef}
 				src={href}
 				frameBorder={0}
